61b9396428af746dc782b90ed1f81db1d002a9cb
zakariabijoy <zakaria.bijoy@live.com>
2020-04-02 15:22:38 +0600

    Upsert Page Functionality is  added

---
+++ BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                // create
+                return Page();
+            }
+            //update
+            Book = await _db.Books.FindAsync(id);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    _db.Books.Add(Book);
+                }
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+
+        }
+    }
+}
